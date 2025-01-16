6e99f94e17b6a9bba919511b4c84fbb5c6323a59
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2025-01-16 07:14:12 -0300

    add instalação automatica de DAPs

--- neovim/plugins/masonnvindap.lua
+++ neovim/plugins/masonnvindap.lua
@@ -1,8 +1,11 @@
 return {
   "jay-babu/mason-nvim-dap.nvim",
-  -- lazy = false,
+  lazy = false,
   config = function()
     require("mason").setup()
-    require("mason-nvim-dap").setup()
+    require("mason-nvim-dap").setup({
+      ensure_installed = { "python", "delve" },
+      automatic_installation = true
+    })
   end,
 }
