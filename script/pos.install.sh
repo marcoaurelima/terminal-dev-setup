3b2ef645eefdc48328c035adbef59b4f91c82ea6
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2025-07-20 16:32:28 -0300

    correção do script de aliases

--- script/pos.install.sh
+++ script/pos.install.sh
@@ -3,5 +3,5 @@
 pos_install() {
     echo -e "alias c=clear\nalias C=clear" >> ~/.zshrc
     echo -e "alias x=exit\nalias X=exit" >> ~/.zshrc
-    echo -e "alias gr=\"go run .\" >>" ~/.zshrc
+    echo -e "alias gr=\"go run .\"" >> ~/.zshrc
 }
