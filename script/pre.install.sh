2539b47d00d3e717e191cb95a4bae4b63b9573de
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2025-07-20 15:17:26 -0300

    add instalação de nerd fonts via pacman

--- script/pre.install.sh
+++ script/pre.install.sh
@@ -9,4 +9,11 @@ pre_install() {
     # xclip para neovim acessar area de transferencia
     echo -e "\n\n\033[30;42m Instalando [xclip] \033[0m"
     sudo pacman -S xclip --noconfirm
+
+    # Instalação de nerdfonts
+    echo -e "\n\n\033[30;42m Instalando [nerd-fonts] \033[0m"
+    sudo pacman -S ttf-martian-mono-nerd --noconfirm
+    sudo pacman -S ttf-jetbrains-mono-nerd --noconfirm
+    sudo pacman -S ttf-bigblueterminal-nerd --noconfirm
+    sudo pacman -S otf-hermit-nerd --noconfirm
 }
